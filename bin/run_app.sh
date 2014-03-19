f04d8989b50ff23ae9484b2a23c0ef5c580aabd4
Michael Quinn <mikejquinn@gmail.com>
2014-03-19 11:07:47 -0400

    Run uberjar in production

--- bin/run_app.sh
+++ bin/run_app.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -f environment.sh ]; then
   source environment.sh
 fi
 
-lein ring server-headless
+if [ "$RING_ENV" = "production" ]; then
+  java -jar target/box-namer-0.1.0-SNAPSHOT-standalone.jar
+else
+  lein ring server-headless
+fi
